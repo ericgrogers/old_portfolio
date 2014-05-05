30a8a191d0247ee8daf38d3b2f85a94c5fe18421
ericgrogers <ericgrogers@fullsail.edu>
2014-05-05 00:14:29 -0500

    +2

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@
  */
 
 (function($){
-    $('h1').fadeIn(2000);
-    $('li').fadeIn(1200);
+    $('h1').fadeIn(1000);
+    $('#about').fadeIn(1200);
+    $('#skills').fadeIn(1400);
+    $('#contact').fadeIn(1600);
 })(jQuery);
