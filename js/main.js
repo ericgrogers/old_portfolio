f6c6763fcb415120be9ab08eddc75bd5d481a3ab
ericgrogers <ericgrogers@fullsail.edu>
2014-11-04 00:44:30 -0600

    added a lot of site styles.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,30 @@
 (function($){
+
+    //============== Page Load ====================================>
+    var pageLoad = function(){
+        var header = $('header');
+        var h1 = $('h1');
+        var h2 = $('h2');
+        var downArrow = $('.down-arrow');
+
+        h1.animate({opacity: 0}, 0);
+        h2.animate({opacity: 0}, 0);
+        downArrow.animate({opacity: 0}, 0);
+
+        setTimeout(function(){
+            h1.addClass('bigEntrance').animate({opacity: 1}, 0);
+        }, 500);
+        setTimeout(function(){
+            h2.addClass("fadeIn").animate({opacity: 1}, 0);
+        },1000);
+        setTimeout(function(){
+            downArrow.addClass("expandUp").animate({opacity: 1}, 0);
+        },1500);
+
+    };
+
+
+    //============= PROJECTS FUNCTIONALITY =========================>
     var images = [1,2,3];
 
     var hideImages = function(){
@@ -60,6 +86,26 @@
         }
     };
 
+    //==================== END PROJECTS FUNCTIONALITY =====================>
+
+    //============== H2 WORD BANNER ===============>
+    var wordCycle = function(){
+        var words = ["Cutting Edge", "Problem Solver", "Code Junkie", "Designer / Developer"];
+        var i = 0;
+        setInterval(function(){
+            var word = words[i];
+            $('#word-banner').animate({opacity: 0}, 600, function(){
+                $(this).html(word);
+            }).animate({opacity: 1}, 600);
+            if(i == words.length -1){
+                i = 0
+            }else{
+                i++;
+            }
+        }, 5000);
+    };
+
+    // ========== SMOOTH SCROLLING ============>
     function smoothScroll(){
         $('a[href*=#]:not([href=#])').on('click', function(e) {
             if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -78,10 +124,15 @@
         });
     }
 
+
+    //============== INIT ==================>
     function init(){
+        pageLoad();
         hideImages();
         showImages(images);
         smoothScroll();
+        wordCycle();
+
 
         // event listeners
         $('#forward-button').on('click', function(e){
